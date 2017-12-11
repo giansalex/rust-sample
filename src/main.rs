85f4e021192d6d216eeced187c5dce6069e7dc31
giansalex <giansalex@gmail.com>
2017-12-11 11:06:25 -0500

    add rand

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 extern crate regex;
+extern crate rand;
+
 mod regex_utils;
+
 
 fn main() {
     regex_utils::regex_work();
+
+    let tuple = rand::random::<(f64, char)>();
+    println!("{:?}", tuple);
+
     println!("Hello, world!");
 }
