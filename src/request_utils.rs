ede5bc0923e1567220c4eaafc3b8bc064379aa9d
giansalex <giansalex@gmail.com>
2017-12-13 13:06:09 -0500

    add test

--- src/request_utils.rs
+++ src/request_utils.rs
@@ -3,12 +3,12 @@ extern crate requests;
 use requests::ToJson;
 
 pub fn run() {
-    let response = requests::get("http://httpbin.org/get").unwrap();
+    let response = requests::post("http://httpbin.org/post").unwrap();
     assert_eq!(response.status_code(), requests::StatusCode::Ok);
 
     let data = response.json().unwrap();
 
-    assert_eq!(data["url"], "http://httpbin.org/get");
+    assert_eq!(data["url"], "http://httpbin.org/post");
     assert_eq!(data["headers"]["Host"], "httpbin.org");
 
     println!("Url {}", data["url"]);
--- tests/simple-tests.rs
+++ tests/simple-tests.rs
@@ -1,8 +1,15 @@
 #[cfg(test)]
 mod tests {
+    extern crate requests;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
         assert_eq!(3 * 6, 18);
+    }
+    #[test]
+    fn requests() {
+        let response = requests::get("http://httpbin.org/get").unwrap();
+        assert_eq!(response.status_code(), requests::StatusCode::Ok);
     }
 }
